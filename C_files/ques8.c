62c9ae7e815668aa09af99e16c2aeed240b79142
ric1234 <richard.noronha@rediffmail.com>
2016-09-14 02:32:42 -0600

    Updated according to hw2

--- C_files/ques8.c
+++ C_files/ques8.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
 #include<string.h>
+#include "hw1.h"
+#include "hw1.c"
 void main()
 {	
 	char strng[30];
@@ -9,30 +11,9 @@ void main()
 	gets(strng);				//takes input from user
 	printf("\n");
 	int len=strlen(strng);
-	//printf("The string is: %s \n", strng);
-	//printf("The length of the string is: %d \n", len);
+	printf("The string is: %s \n", strng);
+	printf("The length of the string is: %d \n", len);
+	rev = reverse(strng, len);		//Function call
 	
-	rev = reverse(strng, len);
-	printf("\n %c \n", rev);
-
 }
 
-char reverse(char * str, int length)
-{
-	char revs[30];
-	int var;
-	if(length>30)		//string is too long
-		return '1';
-	else if(str=="\0")	//Null string
-		return '1';
-	else
-		{	var= length;			//A variable used in reversing
-			for(int i=0; i<length; i++)	//Code for reversing (not printed)
-			{
-				revs[i]=str[var-1];
-				var-=1;
-			}
-			//revs[length]="\0";		//NULL character at the end
-			return '0';	
-		}
-} 
